599cbfb342d0d23acf4556c8c10c8cd5846bc0be
Chandrakanth Nakka <122898582+ChandraakaanthN@users.noreply.github.com>
2024-08-19 22:32:57 +0530

    Update LAB2_8BITADDITION.asm

--- LAB2_8BITADDITION.asm
+++ LAB2_8BITADDITION.asm
@@ -2,7 +2,6 @@ org 100h   ;start address of the program
  ;define two 8bit numbers to add
 num1 db 24h  ;first number (hexadecimal)
 num2 db 29h  ;second number (hexadecimal)
-
 start:  mov al, num1;load the first number into register AL
         add al, num2;Add the second number AL
         ;result is now in AL
@@ -16,26 +15,22 @@ start:  mov al, num1;load the first number into register AL
         add ah,30h   ;convert to ASCII digit(0-9)
         cmp ah,39h     
         jle print_first_digit
-        add ah, 7 ;convert to ASCII letter (A-F) if necessary
-        
+        add ah, 7 ;convert to ASCII letter (A-F) if necessary   
         print_first_digit:
           mov dl,ah ;move the first digit to DL for printing
           mov ah, 02h ;BIOS interrupt to display character 
-          int 21h
-          
+          int 21h     
           ;convert the lower nibble of AL to a character
           mov ah, bl
           and ah, 0Fh ;mask the upper nibble
           add ah, 30h ;convert to ASCII digit(0-9)
           cmp ah, 39h 
           jle print_second_digit
-          add ah, 7 ;convert to ASCII letter (A-F) necessary
-          
+          add ah, 7 ;convert to ASCII letter (A-F) necessary     
         print_second_digit:
         mov dl, ah ;move the second digit to DL for printing
         mov ah ,02h ;BIOS interrupt to display character
         int 21h
-        
         ; end the program
         mov ah, 4Ch ;terminate the program
         int 21h
