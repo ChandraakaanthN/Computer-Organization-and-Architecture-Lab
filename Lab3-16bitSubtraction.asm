5e0738cec057841b8b40c931b518ecf1f485fbd4
Chandrakanth Nakka <122898582+ChandraakaanthN@users.noreply.github.com>
2024-11-21 22:40:54 +0530

    Update Lab3-16bitSubtraction.asm

--- Lab3-16bitSubtraction.asm
+++ Lab3-16bitSubtraction.asm
@@ -6,6 +6,7 @@ start:
     mov ax, num1
     sub ax, num2  
     mov bx, ax
+
     mov ah, al
     and ah, 0F0h    
     shr ah, 4       
@@ -13,20 +14,24 @@ start:
     cmp ah, 39h
     jle print_first_digit
     add ah, 7      
+
 print_first_digit:
     mov dl, ah    
     mov ah, 02h    
-    int 21h         
+    int 21h        
+
     mov ah, bl
     and ah, 0Fh    
     add ah, 30h   
     cmp ah, 39h
     jle print_second_digit
     add ah, 7       
+
 print_second_digit:
     mov dl, ah      
     mov ah, 02h    
     int 21h
+
     mov ah, bh
     and ah, 0F0h 
     shr ah, 4    
@@ -34,20 +39,24 @@ print_second_digit:
     cmp ah, 39h
     jle print_third_digit
     add ah, 7   
+
 print_third_digit:
     mov dl, ah      
     mov ah, 02h   
-    int 21h         
+    int 21h       
+
     mov ah, bl
     and ah, 0Fh     
     add ah, 30h     
     cmp ah, 39h
     jle print_fourth_digit
     add ah, 7      
+
 print_fourth_digit:
     mov dl, ah    
     mov ah, 02h     
-    int 21h      
+    int 21h   
+
     mov ah, 4Ch     
     int 21h
 ret
