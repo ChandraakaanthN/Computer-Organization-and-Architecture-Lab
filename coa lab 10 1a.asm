88d09d3a683a386e6a4476f6e86424a665cbb08b
Chandrakanth Nakka <122898582+ChandraakaanthN@users.noreply.github.com>
2024-11-26 22:17:46 +0530

    Update coa lab 10 1a.asm

--- coa lab 10 1a.asm
+++ coa lab 10 1a.asm
@@ -4,6 +4,7 @@ MOV CX, 10         ; Set loop counter to 26 (for letters 'Z' to 'A')
 MOV AL, '0'        ; Initialize AL with ASCII code for 'Z'
 
 PrintLoop:
+
     MOV DL, AL     ; Move the current character to DL (for printing)
     MOV AH, 02h    ; DOS interrupt function to print a character
     INT 21h        ; Call DOS interrupt
